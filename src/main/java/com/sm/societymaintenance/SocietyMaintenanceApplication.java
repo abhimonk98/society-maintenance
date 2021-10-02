09d48738779f9161fa0fb62d0b09a2246cd86093
Abhijeet Khedekar <abhijeet.khedekar17@vit.edu>
2021-10-02 20:34:18 +0530

    SCM-1: Adding initial commit

---
+++ src/main/java/com/sm/societymaintenance/SocietyMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.sm.societymaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyMaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/societymaintenance/SocietyMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.societymaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
